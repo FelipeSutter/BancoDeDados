1d09ac0704c40d4bd2f32cd4ced3cc0a68f4c18f
Felipe Sutter <lipeps61@gmail.com>
2023-08-18 16:59:14 -0300

    Adição de scripts para teste.

--- Script-1.sql
+++ Script-1.sql
@@ -1,30 +1,51 @@
---
-select
-	*
-from
-	language l
-inner join film f on
-	l.language_id = f.language_id
-order by 
-	f.title asc;
---
 
 select
-	*
+	count (*)
 from
-	language;
---
-select
-f.title,
-f.description,
-f.release_year,
-l.name
-from
-	language l
+	actor a
+inner join film_actor fa on
+	a.actor_id = fa.actor_id
 inner join film f on
-	l.language_id = f.language_id;
---
+	fa.film_id = f.film_id
+where
+	f.title = 'Autumn Crow';
+
+-- esse código conta a qtd de atores no filme descrito.
+-- tem que fazer o inner join duas vezes quando tem uma tabela intermediária no caminho
+-- no exemplo, vai de actor para film_actor e de film_actor para film.
+
+------
+
 select
-	*
+	distinct c.category_id, c.name 
 from
-	film_actor f;
+	film f
+inner join film_category fc on
+	f.film_id  = fc.film_id
+inner join category c on
+	fc.category_id  = c.category_id
+where f.length = 60;
+
+/* Mostra as categorias e o nome delas nos filmes que tem duração = 60 minutos.
+ */
+
+------
+
+select
+	f.title as "Título", concat(a.first_name,' ', a.last_name) as "Nome Completo"
+from
+	actor a
+inner join film_actor fa on
+	a.actor_id = fa.actor_id
+inner join film f on
+	fa.film_id = f.film_id
+where
+	f.title = 'Banger Pinocchio';
+
+/* esse código mostra o título do filme e o primeiro e último nome dos atores nesse filme em específico
+ tem que fazer o inner join duas vezes quando tem uma tabela intermediária no caminho
+ no exemplo, vai de actor para film_actor e de film_actor para film.
+ 
+ concat -- concatena os objetos, duas ou mais colunas
+ as -- renomeia a tabela, colocar o nome entre aspas duplas. */
+	
